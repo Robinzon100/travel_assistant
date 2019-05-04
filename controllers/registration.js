e61cf11e086c4e4f08e6ab7829503e0824dca8b4
Robinzon <robinzon@localhost.localdomain>
2019-05-04 17:04:05 +0400

    changing to express-validator

--- controllers/registration.js
+++ controllers/registration.js
@@ -22,93 +22,29 @@ exports.getRegistration = (req, res, next) => {
 //! POST the /singlogin AND validate user and save user
 // === === === === ===
 exports.postRegistration = (req, res, next) => {
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-    const repeatpassword = req.body.repeatpassword;
-
+    const { username, email, password, repeatpassword } = req.body;
     //? log errors
     let errors = [];
 
-    Joi.validate(
-        { username, email, password, repeatpassword },
-        schema.userSchema,
-        (err, value) => {
-            if (err === null) {
-                //* if the registration GOES WELL
-                const user = new Users(username, email, password);
-                console.log("registration succsess");
-                user.save()
-                    .then(() => {
-                        res.redirect("/tours");
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
-
-                //* if the registration FAILS
-            } else {
-                console.log("registration fail");
-                //? check if the info is full
-                if ( username == " " || email == " " || password == " " || repeatpassword == " ") {
-                    errors.push("please enter in the fields");
-                } else if ( username.trim() !== "" || email.trim() !== "" || password.trim() !== "" || repeatpassword.trim() !== "") {
-                    errors.push("please enter whithout spaces");
-                }
-
-                //? checks the password and username TODO:LENGTH
-                if (password.length <= 4 ) {
-                    errors.push("password too short");
-                }else if (password.length >= 30 ) {
-                    errors.push("password too long");                    
-                }
-
-                if (username.length <= 4 ) {
-                    errors.push("username too short");
-                }else if (username.length >= 30 ) {
-                    errors.push("username too long");                    
-                }
-
-                //? check if the passwords matches
-                if (password != repeatpassword) {
-                    errors.push("the passwords doesnt match");
-                }
-
-                //? check if there are TODO:NUMBERS
-                function isNumeric(n) {
-                    return !isNaN(parseFloat(n)) && isFinite(n);
-                }
-
-                if (isNumeric(password)) {
-                    errors.push("the password must contain at least one number");
-                }
-
-                if (password.match(/\d+/g) == null) {
-                    errors.push("the password must contain at least one number");
-                }
-
-                //? check if the ERROR array is emty
-                if (errors.length > 0) {
-                    res.render("register", {
-                        pageTitle: "travel assistant",
-                        path: "/register",
-                        errors: errors
-                    });
-                }
-                // check if the ERROR array is emty SO THAT IT CAN CONTINUE IN DB
-                else {
-                    console.log("registration succsess");
-                    const user = new Users(username, email, password);
-
-                    user.save()
-                        .then(() => {
-                            res.redirect("/tours");
-                        })
-                        .catch(err => {
-                            console.log(err);
-                        });
-                }
-            }
-        }
-    ); // err === null -> valid
+    // Joi.validate(
+    //     { username, email, password, repeatpassword },
+    //     schema.userSchema,
+    //     (err, value) => {
+    //         if (err === null) {
+    //             //* if the registration GOES WELL
+    //             const user = new Users(username, email, password);
+    //             console.log("registration succsess");
+    //             user.save()
+    //                 .then(() => {
+    //                     res.redirect("/tours");
+    //                 })
+    //                 .catch(err => {
+    //                     console.log(err);
+    //                 });
+    //         } else {
+    //             console.log(err);
+    //         }
+    //     }
+    // );
+ 
 };
--- models/schemas/user.js
+++ models/schemas/user.js
@@ -1,24 +1,26 @@
-const Joi = require("@hapi/joi");
+// const Joi = require("@hapi/joi");
 
-exports.userSchema = Joi.object().keys({
-    username: Joi.string()
-        .required()
-        .alphanum()
-        .min(4)
-        .max(30)
-        .error(errors => {
-            return {
-                message: "Coins is required."
-            };
-        }),
+// exports.userSchema = Joi.object().keys({
+//     username: Joi.string()
+//         .required()
+//         .alphanum()
+//         .min(4)
+//         .max(30)
+//         .error(new Error('username must be valide')),
 
-    email: Joi.string()
-        .required()
-        .email({ minDomainSegments: 2 }),
-    password: Joi.string()
-        .required()
-        .regex(/^[a-zA-Z0-9]{5,30}$/),
-    repeatpassword: Joi.string()
-        .required()
-        .regex(/^[a-zA-Z0-9]{5,30}$/)
-});
+//     email: Joi.string()
+//         .required()
+//         .email({ minDomainSegments: 2 })
+//         .error(new Error('username must be valide')),
+
+//     password: Joi.string()
+//         .required()
+//         .regex(/^[a-zA-Z0-9]{5,30}$/)
+//         .min(4)
+//         .max(30)
+//         .error(new Error('username must be valide')),
+
+//     repeatpassword: Joi.string()
+//         .required()
+//         .regex(/^[a-zA-Z0-9]{5,30}$/),
+// });
