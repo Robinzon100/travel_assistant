be6ef6b924cc227d1409e6f2457ab611a8c51f6f
Robinzon <robinzon@localhost.localdomain>
2019-05-04 15:42:05 +0400

    addded error message and if express validators wount work than ill use this

--- models/schemas/user.js
+++ models/schemas/user.js
@@ -1,15 +1,24 @@
-const Joi = require('@hapi/joi');
-
+const Joi = require("@hapi/joi");
 
 exports.userSchema = Joi.object().keys({
-    username: Joi.string().required().alphanum().min(4).max(30).error(err ={
-        return{
-            message: "please enter minimum of 4 and maximum of 30 carectors"
-        };
-    }),
-    email: Joi.string().required().email({ minDomainSegments: 2 }),
-    password: Joi.string().required().regex(/^[a-zA-Z0-9]{5,30}$/),
-    repeatpassword: Joi.string().required().regex(/^[a-zA-Z0-9]{5,30}$/)
-});
+    username: Joi.string()
+        .required()
+        .alphanum()
+        .min(4)
+        .max(30)
+        .error(errors => {
+            return {
+                message: "Coins is required."
+            };
+        }),
 
- 
+    email: Joi.string()
+        .required()
+        .email({ minDomainSegments: 2 }),
+    password: Joi.string()
+        .required()
+        .regex(/^[a-zA-Z0-9]{5,30}$/),
+    repeatpassword: Joi.string()
+        .required()
+        .regex(/^[a-zA-Z0-9]{5,30}$/)
+});
